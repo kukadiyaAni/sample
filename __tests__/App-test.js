e281a7413155c6ca87e7563363cec19cb1b22a16
iMac <>
2022-04-26 09:53:40 +0530

    test 1

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -10,5 +10,6 @@ import App from '../App';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  let rendered = renderer.create(<App />).toJSON()
+  expect(rendered).toMatchSnapshot()
 });
